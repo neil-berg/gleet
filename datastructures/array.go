e462f12289cf110cf1560d29dbd50721fc62839a
Neil Berg <neil.berg14@gmail.com>
2021-11-20 14:32:49 -0800

    structure: array

---
+++ datastructures/array.go
@@ -0,0 +1,46 @@
+package datastructures
+
+import (
+	"math"
+)
+
+// Array is a list with push, pop, insertAt, and reverse methods
+type Array struct {
+	items []interface{}
+	len   int
+}
+
+// Push adds an item to the right of the array
+func (a *Array) Push(item interface{}) *Array {
+	a.items = append(a.items, item)
+	a.len++
+	return a
+}
+
+// Pop removes the last item in the array
+func (a *Array) Pop() *Array {
+	a.items = a.items[:len(a.items)-1]
+	a.len--
+	return a
+}
+
+// InsertAt adds an item at a certain index
+func (a *Array) InsertAt(item interface{}, i int) *Array {
+	items1 := append(a.items[0:i], item)
+	items2 := a.items[i+1:]
+	a.items = append(items1, items2...)
+	a.len++
+	return a
+}
+
+// Reverse reverses the items
+func (a *Array) Reverse() *Array {
+	f := int(math.Floor(float64(a.len / 2)))
+
+	for i := 0; i < f; i++ {
+		first := a.items[i]
+		second := a.items[a.len-1-i]
+		a.items[i], a.items[a.len-1-i] = second, first
+	}
+	return a
+}
---
+++ datastructures/array_test.go
@@ -0,0 +1,123 @@
+package datastructures
+
+import (
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	arr := &Array{}
+
+	startIdx := 0
+	endIdx := 5
+	for i := startIdx; i < endIdx; i++ {
+		arr.Push(i)
+	}
+
+	tests := map[string]struct {
+		want int
+		got  int
+	}{
+		"length":        {want: endIdx, got: arr.len},
+		"initial value": {want: startIdx, got: arr.items[0].(int)},
+		"last value":    {want: endIdx - 1, got: arr.items[arr.len-1].(int)},
+	}
+
+	for title, test := range tests {
+		t.Run(title, func(t *testing.T) {
+			if test.got != test.want {
+				t.Errorf("want %v, got %v", test.want, test.got)
+			}
+		})
+	}
+}
+
+func TestPop(t *testing.T) {
+	arr := &Array{}
+
+	startIdx := 0
+	endIdx := 10
+	for i := startIdx; i < endIdx; i++ {
+		arr.Push(i)
+	}
+
+	nPop := 4
+	for i := 0; i < nPop; i++ {
+		arr.Pop()
+	}
+
+	tests := map[string]struct {
+		want int
+		got  int
+	}{
+		"length":        {want: endIdx - nPop, got: endIdx - nPop},
+		"initial value": {want: startIdx, got: arr.items[0].(int)},
+		"last value":    {want: endIdx - 1 - nPop, got: arr.items[arr.len-1].(int)},
+	}
+
+	for title, test := range tests {
+		t.Run(title, func(t *testing.T) {
+			if test.got != test.want {
+				t.Errorf("want %v, got %v", test.want, test.got)
+			}
+		})
+	}
+}
+
+func TestInsertAt(t *testing.T) {
+	arr := &Array{}
+
+	startIdx := 0
+	endIdx := 10
+	for i := startIdx; i < endIdx; i++ {
+		arr.Push(i)
+	}
+
+	insertVal := 25
+	insertIdx := 4
+	arr.InsertAt(insertVal, insertIdx)
+
+	tests := map[string]struct {
+		want int
+		got  int
+	}{
+		"length":   {want: endIdx - 1, got: endIdx - 1},
+		"inserted": {want: insertVal, got: arr.items[insertIdx].(int)},
+	}
+
+	for title, test := range tests {
+		t.Run(title, func(t *testing.T) {
+			if test.got != test.want {
+				t.Errorf("want %v, got %v", test.want, test.got)
+			}
+		})
+	}
+}
+
+func TestReverse(t *testing.T) {
+	arr := &Array{}
+
+	startIdx := 0
+	endIdx := 9
+	for i := startIdx; i < endIdx; i++ {
+		arr.Push(i)
+	}
+
+	arr.Reverse()
+
+	tests := map[string]struct {
+		want int
+		got  int
+	}{
+		"length":        {want: endIdx, got: arr.len},
+		"initial value": {want: endIdx - 1, got: arr.items[0].(int)},
+		"last value":    {want: startIdx, got: arr.items[arr.len-1].(int)},
+	}
+
+	for title, test := range tests {
+		t.Run(title, func(t *testing.T) {
+			if test.got != test.want {
+				t.Errorf("want %v, got %v", test.want, test.got)
+			}
+		})
+	}
+}
