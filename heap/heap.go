26dd78f0d051fc1c74a30edb6edb40e15142f5ac
Neil Berg <neil.berg14@gmail.com>
2021-12-02 09:14:26 -0800

    heap: min heapify

--- heap/heap.go
+++ heap/heap.go
@@ -5,26 +5,41 @@ type Heap struct {
 	arr []int
 }
 
+// LeftChildIdx returns the index of the left child for a node at index i
+func (h *Heap) LeftChildIdx(i int) int {
+	return 2*i + 1
+}
+
+// RightChildIdx returns the index of the right child for a node at index i
+func (h *Heap) RightChildIdx(i int) int {
+	return 2*i + 2
+}
+
+// Swap swaps the first and second elements in the heap array
+func (h *Heap) Swap(first, second int) {
+	h.arr[first], h.arr[second] = h.arr[second], h.arr[first]
+}
+
 // MaxHeapify converts a 0-based array into a max-heap
 func (h *Heap) MaxHeapify(i int) {
 	// For node at index i:
-	leftChildIdx := 2*i + 1
-	rightChildIdx := 2*i + 2
+	leftChildIdx := h.LeftChildIdx(i)
+	rightChildIdx := h.RightChildIdx(i)
 
 	// Track the index of the largest value between current parent node (i-th
 	// index) and its children
 	largestIdx := i
 
-	if leftChildIdx < len(h.arr) && h.arr[leftChildIdx] > h.arr[i] {
+	if leftChildIdx < len(h.arr) && h.arr[leftChildIdx] > h.arr[largestIdx] {
 		largestIdx = leftChildIdx
 	}
-	if rightChildIdx < len(h.arr) && h.arr[rightChildIdx] > h.arr[i] {
+	if rightChildIdx < len(h.arr) && h.arr[rightChildIdx] > h.arr[largestIdx] {
 		largestIdx = rightChildIdx
 	}
 
 	if largestIdx != i {
 		// Swap the parent with its larger child node
-		h.arr[i], h.arr[largestIdx] = h.arr[largestIdx], h.arr[i]
+		h.Swap(i, largestIdx)
 		// Recursively heapify (downward) possibly affected subtrees after the swap
 		h.MaxHeapify(largestIdx)
 	}
@@ -36,5 +51,32 @@ func (h *Heap) CreateMaxHeap() {
 	// Last non-leaf node index in a heap is N/2-1
 	for i := len(h.arr)/2 - 1; i >= 0; i-- {
 		h.MaxHeapify(i)
+	}
+}
+
+// MinHeapify converts a 0-based array into a min-heap
+func (h *Heap) MinHeapify(i int) {
+	rightChildIdx := h.RightChildIdx(i)
+	leftChildIdx := h.LeftChildIdx(i)
+
+	smallestIdx := i
+	if leftChildIdx < len(h.arr) && h.arr[leftChildIdx] < h.arr[smallestIdx] {
+		smallestIdx = leftChildIdx
+	}
+	if rightChildIdx < len(h.arr) && h.arr[rightChildIdx] < h.arr[smallestIdx] {
+		smallestIdx = rightChildIdx
+	}
+
+	if smallestIdx != i {
+		h.Swap(i, smallestIdx)
+		h.MinHeapify(smallestIdx)
+	}
+}
+
+// CreateMinHeap reverse walks an array, starting at the last non-leaf node
+// index (i.e. the first parent node index) and turns it into a min-heap.
+func (h *Heap) CreateMinHeap() {
+	for i := len(h.arr)/2 - 1; i >= 0; i-- {
+		h.MinHeapify(i)
 	}
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -15,3 +15,14 @@ func TestCreateMaxHeap(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func TestCreateMinHeap(t *testing.T) {
+	heap := &Heap{arr: []int{5, 10, 3, 8, 9}}
+	heap.CreateMinHeap()
+	got := heap.arr
+	want := []int{3, 8, 5, 10, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
