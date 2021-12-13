6783b58c76c331651c8fba1d355e4fcb308f7cc8
Neil Berg <neil.berg14@gmail.com>
2021-12-13 08:32:25 -0800

    hash table: tests

--- hash/hash_table_test.go
+++ hash/hash_table_test.go
@@ -1,51 +1,64 @@
 package hash
 
 import (
-	"fmt"
 	"testing"
 )
 
-// setupTable is a local helper to setup a hash table that looks like
-// 0: Sam ->
-//
-//
-//
-// func seupTable() *Table {
+func TestGetHashCodeAndIndex(t *testing.T) {
+	ht := NewTable(3)
+	code, index := ht.GetHashCodeAndIndex("Ann")
+	wantCode := 285
+	wantIndex := 0
 
-// }
+	if code != wantCode && index != wantIndex {
+		t.Errorf("got: (%v, %v), want: (%v, %v)", code, index, wantCode, wantIndex)
+	}
+}
 
 func TestInsert(t *testing.T) {
 	ht := NewTable(5)
-	ht.Insert("Sam")
-	// ht.Insert("Sam")
-	// ht.Insert("Joe")
-	// ht.Insert("Pam")
-	// ht.Insert("Ben")
-	// ht.Insert("Ann")
-	// ht.Insert("Pete")
-	// ht.Insert("Jan")
-	// ht.Insert("Bill")
-	// ht.Delete("Pam")
-	// ht.Delete("Jane")
-	// ht.Delete("Ann")
-	ht.Display()
+	s := "Sam"
+	code, index := ht.GetHashCodeAndIndex(s)
+	ht.Insert(s)
+
+	gotNode, err := ht.Get(s)
+	gotValue := gotNode.Data.Value
+	gotCode := gotNode.Data.Code
+
+	if gotValue != s && gotCode != code && err != nil {
+		t.Errorf("Stored wrong data, got (%v, %v), want (%v, %v)", gotCode, gotValue, s, index)
+	}
 }
 
-func TestGet(t *testing.T) {
+func TestDelete(t *testing.T) {
 	ht := NewTable(5)
-	ht.Insert("Sam")
-	ht.Insert("Joe")
-	node, _ := ht.Get("Sam")
-	fmt.Println(node.Data.Value)
-	// ht.Display()
+	s := "Sam"
+	ht.Insert(s)
+	ht.Delete(s)
+
+	gotNode, err := ht.Get(s)
+
+	if gotNode != nil && err == nil {
+		t.Errorf("Failed to delete node")
+	}
 }
 
 func TestResize(t *testing.T) {
+	numBuckets := 5
 	ht := NewTable(5)
 	ht.Insert("Sam")
 	ht.Insert("Joe")
 	ht.Insert("Pam")
 	ht.Insert("Ben")
-	ht.Insert("Ann")
-	ht.Display()
+	ht.Insert("Ann") // Triggers resize (285)
+
+	if ht.NumBuckets != numBuckets*2 {
+		t.Errorf("Wrong num buckets, got %v, want %v", ht.NumBuckets, numBuckets*2)
+	}
+
+	if ht.NumEntries != 5 {
+		t.Errorf("Wrong num buckets, got %v, want %v", ht.NumEntries, 5)
+	}
+
+	// ht.Display()
 }
